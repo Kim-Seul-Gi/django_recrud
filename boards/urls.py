b53e7d91385dec0fb524fdac19565339681029e9
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-03-12 05:53:19 +0000

    board-comment로 이동하기

--- boards/urls.py
+++ boards/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('', views.index, name='index'), # /boards/ = board:index
     path('new/', views.new, name='new'), # /boards/new/
     # path('create/', views.create, name='create'),
-    path('<int:pk>/detail/', views.detail, name='detail'),
-    path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:board_pk>/detail/', views.detail, name='detail'),
+    path('<int:board_pk>/edit/', views.edit, name='edit'),
     # path('<int:pk>/update/', views.update, name='update'),
-    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:board_pk>/delete/', views.delete, name='delete'),
+    path('<int:board_pk>/comments/', views.comments_create, name='comments_create'),
+    # path('<int:board_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name='comments_delete'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,6 @@
 import pprint
 from django.shortcuts import render, redirect
-from .models import Board
+from .models import Board, Comment
 
 # Create your views here.
 
@@ -33,22 +33,22 @@ def new(request):
 #     board.save()
 #     return redirect('boards:detail', board.pk) # redirect('/boards/') 와 같은 의미이다. 왜냐하면 boards/urls.py 에서 name으로 정의했으므로
      
-def detail(request, pk):
-    board=Board.objects.get(pk=pk)
+def detail(request, board_pk):
+    board=Board.objects.get(pk=board_pk)
     comments = board.comment_set.all()
 
     # 다수의 comments를 가져오기 위해서 board 아이디에 해당하는 모든 comment들을 가져옴.
     return render(request, 'boards/detail.html', {'board':board, 'comments':comments})
     
-def edit(request, pk):
+def edit(request, board_pk):
     if request.method == 'POST': # 기존의 update 역할
-        board=Board.objects.get(pk=pk)
+        board=Board.objects.get(pk=board_pk)
         board.title = request.POST.get('title')
         board.content = request.POST.get('content')
         board.save()
         return redirect('boards:detail', board.pk)
     else:    # 기존의 edit 역할, get 형식은 url로 주고받고 하는 형식?? 이므로 ㅇㅇ
-        board=Board.objects.get(pk=pk)
+        board=Board.objects.get(pk=board_pk)
     return render(request, 'boards/edit.html', {'board':board})
     
 # def update(request, pk):  # 이것을 주석처리 할 때에는 앱/urls.py 에서 해당 함수를 주석처리 해야 한다.
@@ -58,13 +58,24 @@ def edit(request, pk):
 #     board.save()
 #     return redirect('boards:detail', board.pk)
     
-def delete(request, pk):
+def delete(request, board_pk):
     if request.method == 'POST': # 직접 html에서 제출을 눌렀을 경우 - POST 형식
-        board=Board.objects.get(pk=pk)
+        board=Board.objects.get(pk=board_pk)
         board.delete()
         return redirect('boards:index')
     else: # 제출을 누른것이 아니라 url 자체를 실행한 경우>GET 형식이다., ex) boards/1/delete/ 를 실행
-        board=Board.objects.get(pk=pk) # 하면 실행 안되게함ㅎㅎ 
+        board=Board.objects.get(pk=board_pk) # 하면 실행 안되게함ㅎㅎ 
         return redirect('boards:detail', board.pk)
+        
+def comments_create(request, board_pk):
+    # 1. 댓글 달 게시물을 가져온다.
+    board = Board.objects.get(pk=board_pk)
+    # 2. 댓글을 저장한다
+    comment = Comment()
+    comment.content = request.POST.get('content')
+    comment.board = board
+    comment.save()
+    return redirect('boards:detail', comment.board_id)
     
-
+    
+    
